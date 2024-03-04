14c823f0f6e61302605d2f0629389ac6cdafec07
Ugo Chinememerem <ugofranklin22@gmail.com>
2024-03-04 12:08:14 +0100

    add:country selection

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -9,8 +9,9 @@ import "./machine.css";
 import { FaSearch } from "react-icons/fa";
 const dashboard = () => {
   const [startDate, setStartDate] = useState(new Date());
-  const [useLiveLocation, setUseLiveLocation] = useState(false);
+  const [selectedCountry, setSelectedCountry] = useState("");
   const [searchResults, setSearchResults] = useState([]);
+  const [numberOfPeople, setNumberOfPeople] = useState("");
   const handleSubmit = (e) => {
     e.preventDefault();
     // Add logic for handling form submission here
@@ -19,9 +20,9 @@ const dashboard = () => {
     setSearchResults(results);
     console.log("Form submitted!");
   };
-  const handleUseLiveLocation = () => {
-    // Add logic to use live location
-    console.log("Using live location");
+
+  const handleCountryChange = (e) => {
+    setSelectedCountry(e.target.value);
   };
 
   return (
@@ -33,55 +34,78 @@ const dashboard = () => {
               <form onSubmit={handleSubmit}>
                 <div className={styles.wrapper}>
                   <div className={styles.searchcontainer}>
-                    {/* <input
-                      type="text"
-                      className={styles.inputstyle}
-                      placeholder="Search destination"
-                    /> */}
                     <select
-                      id="city"
+                      id="country"
                       className={styles.inputstyle}
-                      name="location"
+                      name="country"
+                      onChange={handleCountryChange}
                       required
                     >
-                      <option value="">Select destination</option>
-                      <option value="Abia">Abia</option>
-                      <option value="Adamawa">Adamawa</option>
-                      <option value="Akwa Ibom">Akwa Ibom</option>
-                      <option value="Anambra">Anambra</option>
-                      <option value="Bauchi">Bauchi</option>
-                      <option value="Bayelsa">Bayelsa</option>
-                      <option value="Benue">Benue</option>
-                      <option value="Borno">Borno</option>
-                      <option value="Cross River">Cross River</option>
-                      <option value="Delta">Delta</option>
-                      <option value="Ebonyi">Ebonyi</option>
-                      <option value="Edo">Edo</option>
-                      <option value="Ekiti">Ekiti</option>
-                      <option value="Enugu">Enugu</option>
-                      <option value="Gombe">Gombe</option>
-                      <option value="Imo">Imo</option>
-                      <option value="Jigawa">Jigawa</option>
-                      <option value="Kaduna">Kaduna</option>
-                      <option value="Kano">Kano</option>
-                      <option value="Katsina">Katsina</option>
-                      <option value="Kebbi">Kebbi</option>
-                      <option value="Kogi">Kogi</option>
-                      <option value="Kwara">Kwara</option>
-                      <option value="Lagos">Lagos</option>
-                      <option value="Nasarawa">Nasarawa</option>
-                      <option value="Niger">Niger</option>
-                      <option value="Ogun">Ogun</option>
-                      <option value="Ondo">Ondo</option>
-                      <option value="Osun">Osun</option>
-                      <option value="Oyo">Oyo</option>
-                      <option value="Plateau">Plateau</option>
-                      <option value="Rivers">Rivers</option>
-                      <option value="Sokoto">Sokoto</option>
-                      <option value="Taraba">Taraba</option>
-                      <option value="Yobe">Yobe</option>
-                      <option value="Zamfara">Zamfara</option>
+                      <option value="">Select country</option>
+                      <option value="nigeria">Nigeria</option>
+                      <option value="canada">Canada</option>
                     </select>
+                    {selectedCountry === "nigeria" && (
+                      <select
+                        id="nigeriaState"
+                        className={styles.inputstyle}
+                        name="nigeriaState"
+                        required
+                      >
+                        <option value="">Select state</option>
+                        <option value="Abia">Abia</option>
+                        <option value="Adamawa">Adamawa</option>
+                        <option value="Akwa Ibom">Akwa Ibom</option>
+                        <option value="Anambra">Anambra</option>
+                        <option value="Bauchi">Bauchi</option>
+                        <option value="Bayelsa">Bayelsa</option>
+                        <option value="Benue">Benue</option>
+                        <option value="Borno">Borno</option>
+                        <option value="Cross River">Cross River</option>
+                        <option value="Delta">Delta</option>
+                        <option value="Ebonyi">Ebonyi</option>
+                        <option value="Edo">Edo</option>
+                        <option value="Ekiti">Ekiti</option>
+                        <option value="Enugu">Enugu</option>
+                        <option value="Gombe">Gombe</option>
+                        <option value="Imo">Imo</option>
+                        <option value="Jigawa">Jigawa</option>
+                        <option value="Kaduna">Kaduna</option>
+                        <option value="Kano">Kano</option>
+                        <option value="Katsina">Katsina</option>
+                        <option value="Kebbi">Kebbi</option>
+                        <option value="Kogi">Kogi</option>
+                        <option value="Kwara">Kwara</option>
+                        <option value="Lagos">Lagos</option>
+                        <option value="Nasarawa">Nasarawa</option>
+                        <option value="Niger">Niger</option>
+                        <option value="Ogun">Ogun</option>
+                        <option value="Ondo">Ondo</option>
+                        <option value="Osun">Osun</option>
+                        <option value="Oyo">Oyo</option>
+                        <option value="Plateau">Plateau</option>
+                        <option value="Rivers">Rivers</option>
+                        <option value="Sokoto">Sokoto</option>
+                        <option value="Taraba">Taraba</option>
+                        <option value="Yobe">Yobe</option>
+                        <option value="Zamfara">Zamfara</option>
+                      </select>
+                    )}
+
+                    {selectedCountry === "canada" && (
+                      <select
+                        id="canadaState"
+                        className={styles.inputstyle}
+                        name="canadaState"
+                        required
+                      >
+                        <option value="">Select province</option>
+                        {/* Add options for Canada provinces */}
+                        <option value="ontario">Ontario</option>
+                        <option value="quebec">Quebec</option>
+                        {/* Add more options as needed */}
+                      </select>
+                    )}
 
                     <DatePicker
                       selected={startDate}
